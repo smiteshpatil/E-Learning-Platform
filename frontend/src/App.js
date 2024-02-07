384ac2d09918362b9288d0b090d8684207d6f372
Smitesh <smiteshp2001@gmail.com>
2024-02-07 15:22:43 +0530

    routes_added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import Navbar from "./components/Navbar";
+import LoginPage from "./components/LoginPage";
+import HomePage from "./components/HomePage";
 function App() {
-  return <div className="App">myApp</div>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
