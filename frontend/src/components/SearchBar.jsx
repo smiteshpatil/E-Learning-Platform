459e30e29824162f9e7d1ff521f9775c8aa37240
Smitesh <smiteshp2001@gmail.com>
2024-02-10 03:49:40 +0530

    searchBar enhanced

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SearchBar = () => {
   const searchBarStyle = {
     display: "flex",
     alignItems: "center",
-    maxWidth: "800PX",
+    maxWidth: "800px",
     margin: "0 auto",
     borderRadius: "25px",
   };
@@ -19,14 +21,68 @@ const SearchBar = () => {
     outline: "none",
   };
 
+  const searchResultStyle = {
+    position: "absolute",
+    top: "9%",
+    left: "22.5%",
+    width: "52%",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    zIndex: "9999",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+  };
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const staticData = [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Pineapple",
+    "Mango",
+    "Grapes",
+  ];
+
+  // Function to handle search
+  const handleSearch = (query) => {
+    const result = staticData.filter((item) =>
+      item.toLowerCase().includes(query.toLowerCase())
+    );
+    setSearchResults(result);
+  };
+
+  // Function to clear search results when the search bar is empty
+  const clearSearchResults = () => {
+    if (searchQuery === "") {
+      setSearchResults([]);
+    }
+  };
+
   return (
-    <div style={searchBarStyle}>
-      <input
-        type="text"
-        placeholder="Search for courses..."
-        style={inputStyle}
-      />
-    </div>
+    <>
+      <div style={searchBarStyle}>
+        <input
+          type="text"
+          placeholder="Search for courses..."
+          style={inputStyle}
+          value={searchQuery}
+          onChange={(e) => {
+            setSearchQuery(e.target.value);
+            handleSearch(e.target.value);
+          }}
+          onBlur={clearSearchResults}
+        />
+      </div>
+      {searchQuery !== "" && ( // Only show search results if searchQuery is not empty
+        <div className="search-content" style={searchResultStyle}>
+          {searchResults.map((result, index) => (
+            <Link key={index}>{result}</Link>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
