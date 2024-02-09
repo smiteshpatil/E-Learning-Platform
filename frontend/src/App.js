aeb301293368382c0a2ca59cb0fc5d6a4c3e72b9
Smitesh <smiteshp2001@gmail.com>
2024-02-09 16:10:36 +0530

    bug_fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,12 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import LoginPage from "./components/LoginPage";
 import HomePage from "./components/HomePage";
-<<<<<<< HEAD
-import ProfilePage from "./components/ProfilePage";
 import ProfilePhoto from "./components/ProfilePhoto";
+import ProfilePage from "./components/ProfilePage";
 import ProfileAccountSecurity from "./components/ProfileAccountSecurity";
 import ProfileCloseAccount from "./components/ProfileCloseAccount";
-
-=======
 import Footer from "./components/Footer";
->>>>>>> ddd8b3f9623d264ff1384d9bdf312aed61e279e3
+
 function App() {
   return (
     <div className="App">
@@ -19,10 +16,16 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/profile" element ={<ProfilePage/>}></Route>
-        <Route path="/profile/photo" element ={<ProfilePhoto/>}></Route>
-        <Route path="/profile/security" element ={<ProfileAccountSecurity/>}></Route>
-        <Route path="/profile/closeAccount" element ={<ProfileCloseAccount/>}></Route>
+        <Route path="/profile" element={<ProfilePage />}></Route>
+        <Route path="/profile/photo" element={<ProfilePhoto />}></Route>
+        <Route
+          path="/profile/security"
+          element={<ProfileAccountSecurity />}
+        ></Route>
+        <Route
+          path="/profile/closeAccount"
+          element={<ProfileCloseAccount />}
+        ></Route>
       </Routes>
       <Footer />
     </div>
