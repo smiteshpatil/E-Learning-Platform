8f1f2482c8474d123fb4d81dbc161869621959c6
Smitesh <smiteshp2001@gmail.com>
2024-02-11 20:47:32 +0530

    -

--- frontend/src/components/pages/Products.jsx
+++ frontend/src/components/pages/Products.jsx
@@ -2,54 +2,51 @@ import { courses as currentCourses } from "../../api/courseService";
 import { CartProvider, useCart } from "react-use-cart";
 import { Link } from "react-router-dom";
 function Page() {
-    const { addItem } = useCart();
-  
-    return (
-      <div>
+  const { addItem } = useCart();
+
+  return (
+    <div>
       <div className="course-content-section">
-            <div className="course-container">
-      {currentCourses.map((currentCourse) => (
-                <div className="course-post" key={currentCourse.id}>
-                
+        <div className="course-container">
+          {currentCourses.map((currentCourse) => (
+            <div className="course-post" key={currentCourse.id}>
+              <img className="cover-img" src={currentCourse.thumbnail} alt="" />
+              <h2 className="title">{currentCourse.title}</h2>
+              <p className="description">{currentCourse.description}</p>
+              <div className="card-details">
+                <div className="lh-details">
                   <img
-                    className="cover-img"
-                    src={currentCourse.thumbnail}
+                    className="author-img"
+                    src={currentCourse.author.profilePicture.url}
                     alt=""
                   />
-                  <h2 className="title">{currentCourse.title}</h2>
-                  <p className="description">{currentCourse.description}</p>
-                  <div className="card-details">
-                    <div className="lh-details">
-                      <img
-                        className="author-img"
-                        src={currentCourse.author.profilePicture.url}
-                        alt=""
-                      />
-                      <p className="date">
-                        {new Date(
-                          `${currentCourse.datePublished}`
-                        ).toLocaleDateString("en-us", {
-                          year: "numeric",
-                          month: "short",
-                          day: "numeric",
-                        })}
-                      </p>
-                    </div>
-                  </div>
-                  <button onClick={() => addItem(currentCourse)}>Add to cart</button>
+                  <p className="date">
+                    {new Date(
+                      `${currentCourse.datePublished}`
+                    ).toLocaleDateString("en-us", {
+                      year: "numeric",
+                      month: "short",
+                      day: "numeric",
+                    })}
+                  </p>
                 </div>
-              ))}
+              </div>
+              <button onClick={() => addItem(currentCourse)}>
+                Add to cart
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
-      </div>
-      </div>
-    );
-  }
-  function AppProducts() {
-    return (
-      <CartProvider>
-        <Page />
-        <Link to={`../cart`}>cart</Link>
-      </CartProvider>
-    );
-  }
-  export default AppProducts;
+    </div>
+  );
+}
+function AppProducts() {
+  return (
+    <CartProvider>
+      <Page />
+      <Link to={`../cart`}>cart</Link>
+    </CartProvider>
+  );
+}
+export default AppProducts;
