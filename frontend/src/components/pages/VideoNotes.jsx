f5a30e27faf9d96f241ebbfd897cc335b26a57d0
Smitesh <smiteshp2001@gmail.com>
2024-02-11 15:21:10 +0530

    VideoPlayer Added

---
+++ frontend/src/components/pages/VideoNotes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VideoNotes = () => {
+  return (
+    <div>
+      <h2>Video Notes</h2>
+      <p>This is the notes section.</p>
+    </div>
+  );
+};
+
+export default VideoNotes;
---
+++ frontend/src/components/pages/VideoQAndA.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VideoQAndA = () => {
+  return (
+    <div>
+      <h2>Video Q/A</h2>
+      <p>This is the Q/A section.</p>
+    </div>
+  );
+};
+
+export default VideoQAndA;
