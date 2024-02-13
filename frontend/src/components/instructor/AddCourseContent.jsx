d4f19c6fb7dae1f1b556e0ac92cdec795ca6f2c9
Smitesh <smiteshp2001@gmail.com>
2024-02-13 23:53:08 +0530

    course creation api added

---
+++ frontend/src/components/instructor/AddCourseContent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddCourseContent = (props) => {
+  return <div>AddCourse Content</div>;
+};
+
+export default AddCourseContent;
--- frontend/src/components/instructor/Courses.jsx
+++ frontend/src/components/instructor/Courses.jsx
@@ -1,31 +1,30 @@
 import React, { useState } from "react";
 import CreateCourseCard from "./CreateCourseCard";
+import MyCourses from "./MyCourses";
+import { createNewCourse } from "../../api/courseService";
 
 const Courses = () => {
   // State for managing the list of courses
-  const [courses, setCourses] = useState([]);
-
-  // State for managing the form input for launching a new course
-  const [newCourseName, setNewCourseName] = useState("");
+  const [myCourses, setMyCourses] = useState([]);
 
   // Function to handle launching a new course
-  const launchNewCourse = () => {
-    if (newCourseName.trim() !== "") {
-      // Here, you would typically send a request to your backend API to create the new course
-      // After successfully creating the course, you would update the courses state
-      // For demonstration purposes, we'll just add the new course to the state directly
-      const newCourse = {
-        id: courses.length + 1,
-        name: newCourseName,
-      };
-      setCourses([...courses, newCourse]);
-      setNewCourseName(""); // Clear input field after launching a new course
+  const launchNewCourse = (newCourse) => {
+    // Send a request to your backend API to create the new course
+    const token = localStorage.getItem("token");
+    try {
+      const resp = createNewCourse(newCourse, token);
+      console.log(resp);
+    } catch (err) {
+      console.log(err);
     }
+
+    setMyCourses([...myCourses, newCourse]);
   };
 
   return (
     <>
-      <CreateCourseCard />
+      <CreateCourseCard createCourse={launchNewCourse} />
+      <MyCourses courses={myCourses} />
     </>
   );
 };
