744f14fe110102ddb592094d3769c93a76b7ff02
Dhananjay-Shelke <dhananjayshelke2703@gmail.com>
2024-02-13 18:06:50 +0530

    added feedback entity

---
+++ backend/src/main/java/com/app/dto/FeedbackDTO.java
@@ -0,0 +1,25 @@
+package com.app.dto;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Max;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class FeedbackDTO {
+    
+    private String comment;
+    
+    @Min(0)
+    @Max(5)
+    private int rating;
+
+    private Long courseId;
+
+    private Long studentId;
+
+}
---
+++ backend/src/main/java/com/app/entities/Feedback.java
@@ -0,0 +1,39 @@
+package com.app.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Max;
+
+@Entity
+@Table(name = "course_feedback")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Feedback extends BaseEntity{
+	
+	@Column(name="comments")
+	private String comment;
+	
+	@Min(0)
+	@Max(5)
+	private int rating;
+	
+
+	@ManyToOne
+	@JoinColumn(name="course_id")
+	private Course course;
+	
+	@ManyToOne
+	@JoinColumn(name="student_id")
+	private Student student;
+	
+
+}
