0568887713785ab4156736a8dddc49b18a24330f
Smitesh <smiteshp2001@gmail.com>
2024-02-18 19:46:01 +0530

    added search feature

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,53 +1,21 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { useAuth } from "../context/AuthContext";
+import "../css/Searchbar.css";
 const SearchBar = () => {
-  const searchBarStyle = {
-    display: "flex",
-    alignItems: "center",
-    width: "50%",
-    maxWidth: "700px",
-    margin: "0",
-    borderRadius: "25px",
-  };
-
-  const inputStyle = {
-    width: "100%",
-    margin: "7px",
-    borderRadius: "5px",
-    border: "1px solid #ccc",
-  };
-
-  const searchResultStyle = {
-    position: "absolute",
-    top: "9%",
-    left: "18.4%",
-    width: "52%",
-    backgroundColor: "rgba(0, 0, 0, 0.4)",
-    zIndex: "9999",
-    justifyContent: "center",
-    alignItems: "center",
-    color: "white",
-  };
+  const { allCourses } = useAuth();
 
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
 
-  const staticData = [
-    "Apple",
-    "Banana",
-    "Orange",
-    "Pineapple",
-    "Mango",
-    "Grapes",
-  ];
-
   // Function to handle search
   const handleSearch = (query) => {
-    const result = staticData.filter((item) =>
-      item.toLowerCase().includes(query.toLowerCase())
+    const result = allCourses.map((each) => each.courseDTO);
+    result.filter((item) =>
+      item.courseName.toLowerCase().includes(query.toLowerCase())
     );
+    console.log(result);
     setSearchResults(result);
   };
 
@@ -60,12 +28,11 @@ const SearchBar = () => {
 
   return (
     <>
-      <div style={searchBarStyle}>
+      <div className="searchBarStyle">
         <input
-          className="form-control"
+          className="form-control inputStyle"
           type="text"
           placeholder="Search..."
-          style={inputStyle}
           value={searchQuery}
           onChange={(e) => {
             setSearchQuery(e.target.value);
@@ -75,9 +42,17 @@ const SearchBar = () => {
         />
       </div>
       {searchQuery !== "" && ( // Only show search results if searchQuery is not empty
-        <div className="search-content" style={searchResultStyle}>
-          {searchResults.map((result, index) => (
-            <Link key={index}>{result}</Link>
+        <div
+          className={`searchResultStyle search-content ${
+            searchResults.length > 0 ? "show" : ""
+          }`}
+        >
+          {searchResults.map((currCourse, index) => (
+            <>
+              <Link to={`/course/`} key={index}>
+                {currCourse.courseName}
+              </Link>
+            </>
           ))}
         </div>
       )}
