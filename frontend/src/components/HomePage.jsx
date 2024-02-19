95ab1f6791113c2dacbced551cf5b941a5f7c900
Smitesh <smiteshp2001@gmail.com>
2024-02-19 13:33:33 +0530

    updated category list

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Carousel from "./Carousel";
 import Main from "./Main";
 import CategoryList from "./pages/CategoryList";
-
+import MyLearning from "./student/MyLearning";
+import { useAuth } from "../context/AuthContext";
 const HomePage = () => {
+  let { authUser } = useAuth();
   return (
     <div>
       <CategoryList />
       <Carousel />
+      {authUser && <MyLearning />}
       <Main />
     </div>
   );
